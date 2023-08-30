c754b27bda8ad9f7f1f1a6848f471d3286e27570
Raymond.Meara-Klinge.Student <112421067+Raymond-Meara-Klinge@users.noreply.github.com>
2023-08-30 16:11:02 +0000

    working on palindrome

--- Program.cs
+++ Program.cs
@@ -6,60 +6,109 @@ namespace NoviceChallenges
     {
         static void Main(string[] args)
         {
-            // Use the Main method to test your functions.
+            int number;
+            int a = 37;
+            int b = 6;
+            int c = -23;
+            string words;
+            string word2;
+            string word3;
+            int[] nums = { 43, 9, 52, 75, 12, 80, 234, 65 };
+
+            Random rnd = new Random();
+            number = rnd.Next(1, 15);
+
+            words = "How could you count me out like that?";
+            word2 = "Hello, how has your day been?";
+            word3 = "racecar";
+
+            Console.WriteLine($"{a} + {b} = {Add(a, b)}");
+            Console.WriteLine($"The number {number} is even: {IsEven(number)}");
+            Console.WriteLine($"The highest number between {a}, {b}, and {c} is {MaxOfThree(a, b, c)}");
+            Console.WriteLine($"The string message, {words}, is {StringLength(words)} words long.");
+            Console.WriteLine($"The string message, {word2}, starts with the word ''Hello'': {StartsHello(word2)}");
+            Console.WriteLine($"The reversed string message of {word2} is: {ReverseString(word2)}");
+            Console.WriteLine($"{number}! = {Factorial(number)}");
+            Console.WriteLine($"{number} is a prime number : {IsPrime(number)}");
+            Console.WriteLine($"The Fibonacci number in the {number}th position is {Fibonacci(number)}");
+            Console.WriteLine($"The largest number in the given integer array is {LargestInArray(nums)}");
+            Console.WriteLine($"The word {word3} is a palindrome: {IsPalindrome(word3)}");
+
         }
 
         // 1. Return the sum of two numbers.
         public static int Add(int a, int b)
         {
-            // TODO: Implement this method.
-            return 0;
+            int total;
+            total = a + b;
+            return total;
         }
 
         // 2. Given an integer, return true if it's even, else return false.
         public static bool IsEven(int number)
         {
-            // TODO: Implement this method.
+            if (number % 2 == 0)
+            {
+                return true;
+            }
             return false;
         }
 
         // 3. Return the largest of three numbers.
         public static int MaxOfThree(int a, int b, int c)
         {
-            // HINT: You might want to use Math.Max function.
-            // TODO: Implement this method.
-            return 0;
+            if (a > b)
+            {
+                return Math.Max(a, c);
+            }
+            return Math.Max(b, c);
         }
 
         // 4. Return the length of the given string.
         public static int StringLength(string s)
         {
-            // TODO: Implement this method.
-            return 0;
+            int total = 0;
+            string[] word = s.Split(' ');
+            foreach (string wor in word)
+            {
+                total++;
+            }
+            return total;
         }
 
         // 5. Return true if the string starts with "Hello", otherwise return false.
         public static bool StartsHello(string s)
         {
-            // HINT: Use the string method "StartsWith".
-            // TODO: Implement this method.
-            return false;
+            return s.StartsWith("Hello");
         }
 
         // 6. Reverse a given string.
         public static string ReverseString(string s)
         {
             char[] charArray = s.ToCharArray();
-            // TODO: Reverse the charArray.
-            return new string(charArray);
+            char[] charArrayz = new char[charArray.Length];
+            int inv = 0;
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                charArrayz[inv] = charArray[i];
+                inv++;
+            }
+            return new string(charArrayz);
         }
 
         // 7. Return the factorial of a number.
         public static int Factorial(int n)
         {
-            if (n == 0) return 1;
-            // TODO: Calculate the factorial.
-            return 0;
+            if (n <= 1) return 1;
+            int total = 1;
+            int[] facts = new int[n];
+            foreach (int fact in facts)
+            {
+                facts[fact] = n;
+                n--;
+                total *= facts[fact];
+            }
+            return total;
         }
 
         // 8. Check if a number is a prime number.
@@ -70,16 +119,23 @@ namespace NoviceChallenges
             {
                 if (number % i == 0) return false;
             }
-            // TODO: Return the correct boolean value.
-            return false;
+            return true;
         }
 
         // 9. Return the nth Fibonacci number.
         public static int Fibonacci(int n)
         {
             if (n <= 1) return n;
-            // TODO: Calculate the nth Fibonacci number.
-            return 0;
+            int num1 = 0;
+            int num2 = 1;
+            int total = 0;
+            for (int i = 2; i < n + 1; i++)
+            {
+                total = num1 + num2;
+                num1 = num2;
+                num2 = total;
+            }
+            return total;
         }
 
         // 10. Given an array of integers, return the largest number.
@@ -88,7 +144,10 @@ namespace NoviceChallenges
             int largest = numbers[0];
             for (int i = 1; i < numbers.Length; i++)
             {
-                // TODO: Find the largest number in the array.
+                if (largest < numbers[i])
+                {
+                    largest = numbers[i];
+                }
             }
             return largest;
         }
@@ -96,8 +155,17 @@ namespace NoviceChallenges
         // 11. Check if a string is a palindrome (reads the same forward and backward).
         public static bool IsPalindrome(string s)
         {
-            // TODO: Determine if the string is a palindrome.
-            return false;
+            char[] word = s.ToCharArray();
+            char[] aword = new char[word.Length];
+            for (int i = 0; i <= word.Length-1; i++)
+            {
+                aword[i] = word[i];
+                if (word[i] != aword[i])
+                {
+                    return false;
+                }
+            }
+                return true;
         }
 
         // 12. Given an array of integers, return the sum of its elements.
